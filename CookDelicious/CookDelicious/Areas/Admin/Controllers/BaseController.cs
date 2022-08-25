5eed3c127915609d65cb4042bc536e700b718933
ilyo_bilyo <ilito20000@gmail.com>
2022-08-25 19:07:40 +0300

    Add Area Admin with views and controllers

---
+++ CookDelicious/CookDelicious/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using CookDelicious.Core.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookDelicious.Areas.Admin.Controllers
+{
+    [Authorize(Roles = UserConstraints.Roles.Administrator)]
+    [Area("Admin")]
+    public class BaseController : Controller
+    {
+       
+    }
+}
---
+++ CookDelicious/CookDelicious/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using CookDelicious.Core.Constants;
+using CookDelicious.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookDelicious.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        private readonly IUserService userService;
+
+        public UserController(RoleManager<IdentityRole> roleManager, IUserService userService)
+        {
+            this.roleManager = roleManager;
+            this.userService = userService;
+        }
+
+        [Authorize(Roles = UserConstraints.Roles.Administrator)]
+        public async Task<IActionResult> ManageUsers()
+        {
+            var users = await userService.GetUsers();
+
+            return View(users);
+        }
+
+
+        [Authorize(Roles = UserConstraints.Roles.Administrator)]
+        public async Task<IActionResult> CreateRole()
+        {
+            await roleManager.CreateAsync(new IdentityRole()
+            {
+                Name = "Administrator"
+            });
+
+            return Redirect("/");
+        }
+    }
+}
