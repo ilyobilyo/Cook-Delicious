3c1ec69c302e666beed747aee7e7e79df40a9ad4
ilyo_bilyo <ilito20000@gmail.com>
2022-08-26 13:58:33 +0300

    Add paging

---
+++ CookDelicious/CookDelicious.Core/Models/Paiging/PagingList.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookDelicious.Core.Models.Paiging
+{
+    public class PagingList<T> : List<T>
+    {
+        public PagingList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public int PageIndex { get; init; }
+
+        public int TotalPages { get; init; }
+
+        public bool HasPreviousPage => PageIndex > 1;
+
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        public static async Task<PagingList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PagingList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
