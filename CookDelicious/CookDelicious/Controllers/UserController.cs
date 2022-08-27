b7a3ecf5688e27241adc804bd08f727ef014dc87
ilyobilyo <ilito20000@gmail.com>
2022-08-27 13:28:49 +0300

    Add UserProfile View and Edit View

--- CookDelicious/CookDelicious/Controllers/UserController.cs
+++ CookDelicious/CookDelicious/Controllers/UserController.cs
@@ -1,16 +1,17 @@
-﻿using CookDelicious.Core.Contracts;
+﻿using CookDelicious.Core.Constants;
+using CookDelicious.Core.Contracts;
 using CookDelicious.Core.Contracts.User;
+using CookDelicious.Core.Models.User;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CookDelicious.Controllers
 {
-    [Authorize(Roles = "User, Administrator")]
+    [Authorize(Roles = "Administrator, User")]
     public class UserController : BaseController
     {
         private readonly IUserService userService;
-
         public UserController(IUserService userService)
         {
             this.userService = userService;
@@ -18,9 +19,36 @@ namespace CookDelicious.Controllers
 
         public async Task<IActionResult> Profile()
         {
-            //var model = userService.GetUserProfile(User.Identity.Name);
+            var model = await userService.GetUserProfile(User.Identity.Name);
 
-            return View();
+            return View(model);
+        }
+
+        public async Task<IActionResult> EditProfile()
+        {
+            var model = await userService.GetUserProfileEdit(User.Identity.Name);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(UserEditProfileViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (await userService.UpdateUser(model))
+            {
+                ViewData[MessageConstant.SuccessMessage] = "Успешен запис!";
+            }
+            else
+            {
+                ViewData[MessageConstant.ErrorMessage] = "Възникна грешка!";
+            }
+
+            return View(model);
         }
     }
 }
