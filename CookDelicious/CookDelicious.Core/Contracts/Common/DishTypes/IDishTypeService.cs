46413b41f2b9f975bc91bb749f1b99b783a34903
ilyobilyo <ilito20000@gmail.com>
2022-08-29 19:02:12 +0300

    Add DishType User Area Service

---
+++ CookDelicious/CookDelicious.Core/Contracts/Common/DishTypes/IDishTypeService.cs
@@ -0,0 +1,9 @@
+﻿using CookDelicious.Infrasturcture.Models.Common;
+
+namespace CookDelicious.Core.Contracts.Common.DishTypes
+{
+    public interface IDishTypeService
+    {
+        Task<DishType> GetDishTypeByName(string dishTypeName);
+    }
+}
---
+++ CookDelicious/CookDelicious.Core/Services/Common/DishTypes/DishTypeService.cs
@@ -0,0 +1,24 @@
+﻿using CookDelicious.Core.Contracts.Common.DishTypes;
+using CookDelicious.Infrasturcture.Models.Common;
+using CookDelicious.Infrasturcture.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookDelicious.Core.Services.Common.DishTypes
+{
+    public class DishTypeService : IDishTypeService
+    {
+        private readonly IApplicationDbRepository repo;
+
+        public DishTypeService(IApplicationDbRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<DishType> GetDishTypeByName(string dishTypeName)
+        {
+            return await repo.All<DishType>()
+                .Where(x => x.Name == dishTypeName)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
