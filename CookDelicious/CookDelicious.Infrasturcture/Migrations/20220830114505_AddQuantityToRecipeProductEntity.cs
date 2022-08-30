9ed5ad76e47d2c4a852b41fee46b3c88e06b5821
ilyobilyo <ilito20000@gmail.com>
2022-08-30 18:04:48 +0300

    Add Quantity of RecipeProduct Table

---
+++ CookDelicious/CookDelicious.Infrasturcture/Migrations/20220830114505_AddQuantityToRecipeProductEntity.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookDelicious.Infrastructure.Data.Migrations
+{
+    public partial class AddQuantityToRecipeProductEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Quantity",
+                table: "RecipeProducts",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "RecipeProducts");
+        }
+    }
+}
