19a77100663743fb9e0c74d13219de2119512986
ilyobilyo <ilito20000@gmail.com>
2022-09-01 17:11:59 +0300

    Add ForumService and Controller for Admin

---
+++ CookDelicious/CookDelicious.Core/Contracts/Admin/IForumServiceAdmin.cs
@@ -0,0 +1,10 @@
+﻿using CookDelicious.Core.Models.Admin.Forum;
+using CookDelicious.Models;
+
+namespace CookDelicious.Core.Contracts.Admin
+{
+    public interface IForumServiceAdmin
+    {
+        Task<ErrorViewModel> CreatePostCategory(CreatePostCategoryViewModel model);
+    }
+}
---
+++ CookDelicious/CookDelicious.Core/Services/Admin/ForumServiceAdmin.cs
@@ -0,0 +1,50 @@
+﻿using CookDelicious.Core.Contracts.Admin;
+using CookDelicious.Core.Models.Admin.Forum;
+using CookDelicious.Infrasturcture.Models.Forum;
+using CookDelicious.Infrasturcture.Repositories;
+using CookDelicious.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookDelicious.Core.Services.Admin
+{
+    public class ForumServiceAdmin : IForumServiceAdmin
+    {
+        private readonly IApplicationDbRepository repo;
+
+        public ForumServiceAdmin(IApplicationDbRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<ErrorViewModel> CreatePostCategory(CreatePostCategoryViewModel model)
+        {
+            if (await IsPostCategoryExists(model))
+            {
+                return new ErrorViewModel() { Messages = $"{model.Name} is already exist." };
+            }
+
+            var postCategory = new PostCategory()
+            {
+                Name = model.Name,
+            };
+
+            try
+            {
+                await repo.AddAsync(postCategory);
+                await repo.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return new ErrorViewModel() { Messages = "Unexpected error. You cant add this post category!" };
+            }
+
+            return null;
+        }
+
+        private async Task<bool> IsPostCategoryExists(CreatePostCategoryViewModel model)
+        {
+            return await repo.All<PostCategory>()
+                .AnyAsync(x => x.Name == model.Name);
+        }
+    }
+}
