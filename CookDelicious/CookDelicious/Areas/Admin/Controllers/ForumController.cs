6c8f3b581a806fccdeae6e963ef2b0d0a2a248c0
ilyobilyo <ilito20000@gmail.com>
2022-09-01 17:10:07 +0300

    Add ForumController

---
+++ CookDelicious/CookDelicious/Areas/Admin/Controllers/ForumController.cs
@@ -0,0 +1,40 @@
+﻿using CookDelicious.Core.Constants;
+using CookDelicious.Core.Contracts.Admin;
+using CookDelicious.Core.Models.Admin.Forum;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookDelicious.Areas.Admin.Controllers
+{
+    public class ForumController : BaseController
+    {
+        private readonly IForumServiceAdmin forumService;
+
+        public ForumController(IForumServiceAdmin forumService)
+        {
+            this.forumService = forumService;
+        }
+
+        public IActionResult CreatePostCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePostCategory(CreatePostCategoryViewModel model)
+        {
+            var error = await forumService.CreatePostCategory(model);
+
+            if (error != null)
+            {
+                ViewData[MessageConstant.ErrorMessage] = error.Messages;
+                return View(model);
+            }
+            else
+            {
+                ViewData[MessageConstant.SuccessMessage] = "Успешен запис";
+            }
+
+            return View();
+        }
+    }
+}
