f84b1e5d091d91808e780a6a84628828a5b3c176
ilyobilyo <ilito20000@gmail.com>
2022-09-01 17:10:42 +0300

    Add ForumViewModels for CreatePost, Home and ForumPost

---
+++ CookDelicious/CookDelicious.Core/Models/Forum/CreatePostViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CookDelicious.Infrasturcture.Models.Forum;
+
+namespace CookDelicious.Core.Models.Forum
+{
+    public class CreatePostViewModel
+    {
+        public string Title { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime PublishedOn { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public IList<string> Categories { get; set; }
+    }
+}
---
+++ CookDelicious/CookDelicious.Core/Models/Forum/ForumHomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CookDelicious.Core.Models.Forum
+{
+    public class ForumHomeViewModel
+    {
+        public IList<string> Categories { get; set; }
+
+        public IList<string> Archive { get; set; }
+
+        public IList<ForumPostViewModel> Posts { get; set; }
+    }
+}
