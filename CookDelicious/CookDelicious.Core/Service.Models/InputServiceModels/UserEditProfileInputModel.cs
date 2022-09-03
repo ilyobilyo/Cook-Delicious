5977e66deefb75cb335ed9ff2b72b39eb62ac4ca
ilyo_bilyo <ilito20000@gmail.com>
2022-09-03 19:44:31 +0300

    Modify User views and add InputServiceModels

---
+++ CookDelicious/CookDelicious.Core/Service.Models/InputServiceModels/UserEditProfileInputModel.cs
@@ -0,0 +1,25 @@
+﻿namespace CookDelicious.Core.Service.Models.InputServiceModels
+{
+    public class UserEditProfileInputModel
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int? Age { get; set; }
+
+        public string Town { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string? Job { get; set; }
+
+        public string? Address { get; set; }
+    }
+}
