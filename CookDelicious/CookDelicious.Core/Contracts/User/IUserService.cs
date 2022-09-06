0759c1132d7320259ea98131ce09ac3e15c22415
ilyo_bilyo <ilito20000@gmail.com>
2022-09-06 16:46:01 +0300

    Add GetApplicationUserByUserName method in UserService

--- CookDelicious/CookDelicious.Core/Contracts/User/IUserService.cs
+++ CookDelicious/CookDelicious.Core/Contracts/User/IUserService.cs
@@ -1,5 +1,6 @@
 ﻿using CookDelicious.Core.Service.Models;
 using CookDelicious.Core.Service.Models.InputServiceModels;
+using CookDelicious.Infrasturcture.Models.Identity;
 
 namespace CookDelicious.Core.Contracts.User
 {
@@ -7,5 +8,6 @@ namespace CookDelicious.Core.Contracts.User
     {
         Task<bool> UpdateUser(UserEditProfileInputModel model);
         Task<UserServiceModel> GetUserByUsername(string author);
+        Task<ApplicationUser> GetApplicationUserByUsername(string username);
     }
 }
