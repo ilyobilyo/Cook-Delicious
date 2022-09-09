dc7e4fe63556ae8f32d90e1d19c607c541bac379
ilyo_bilyo <ilito20000@gmail.com>
2022-09-09 19:15:55 +0300

    Remove CommentController. Create RecipeController

---
+++ CookDelicious/CookDelicious/Areas/Admin/Controllers/RecipeController.cs
@@ -0,0 +1,22 @@
+﻿using CookDelicious.Core.Contracts.Admin;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookDelicious.Areas.Admin.Controllers
+{
+    public class RecipeController : BaseController
+    {
+        private readonly ICommentServiceAdmin commentService;
+
+        public RecipeController(ICommentServiceAdmin commentService)
+        {
+            this.commentService = commentService;
+        }
+
+        public async Task<IActionResult> DeleteRecipeComment([FromRoute] Guid id)
+        {
+            await commentService.DeleteRecipeComment(id);
+
+            return Redirect("/Admin/User/ManageUsers");
+        }
+    }
+}
