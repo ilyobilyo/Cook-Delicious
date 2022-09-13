5321c5d633cf48ff0f4b9a7f5744d96f1ffe2d56
ilyobilyo <ilito20000@gmail.com>
2022-09-13 17:08:15 +0300

    Add BlogHome and Post View models

---
+++ CookDelicious/CookDelicious.Core/Models/Blog/BlogHomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CookDelicious.Core.Models.Paiging;
+
+namespace CookDelicious.Core.Models.Blog
+{
+    public class BlogHomeViewModel
+    {
+        public IList<string> Categories { get; set; }
+
+        public IList<string> Archive { get; set; }
+
+        public PagingList<BlogPostViewModel> Posts { get; set; }
+    }
+}
---
+++ CookDelicious/CookDelicious.Core/Models/Blog/BlogPostViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CookDelicious.Core.Models.Forum;
+
+namespace CookDelicious.Core.Models.Blog
+{
+    public class BlogPostViewModel : PostViewModel
+    {
+    }
+}
