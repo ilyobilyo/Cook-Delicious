41dc6e03142d5549abbbd8437ca9cc74e825ed1b
ilyobilyo <ilito20000@gmail.com>
2022-09-13 17:09:02 +0300

    Add Create BlogPostViewModel for Admin

---
+++ CookDelicious/CookDelicious.Core/Models/Admin/Blog/CreateBlogPostViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CookDelicious.Core.Models.Admin.Blog
+{
+    public class CreateBlogPostViewModel
+    {
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime PublishedOn { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public IList<string> Categories { get; set; }
+    }
+}
