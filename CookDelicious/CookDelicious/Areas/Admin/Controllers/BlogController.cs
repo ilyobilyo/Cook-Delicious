a6e4b07b29b07bcf1a99b8fc3d58538f49ced8e9
ilyobilyo <ilito20000@gmail.com>
2022-09-13 17:09:38 +0300

    Add BlogController in Admin area and CreateBlogPostCategory and CreateBlogPost Views

---
+++ CookDelicious/CookDelicious/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using CookDelicious.Core.Contracts.Admin;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookDelicious.Areas.Admin.Controllers
+{
+    public class BlogController : BaseController
+    {
+        private readonly IBlogServiceAdmin blogService;
+        private readonly IMapper mapper;
+
+        public BlogController(IBlogServiceAdmin blogService, IMapper mapper)
+        {
+            this.blogService = blogService;
+            this.mapper = mapper;
+        }
+
+        public IActionResult CreateBlogPost()
+        {
+            //var categories = await forumService.GetAllPostCategoryNames();
+
+            //var model = new CreatePostViewModel()
+            //{
+            //    Categories = categories,
+            //};
+
+            //return View(model);
+        }
+
+
+
+        public IActionResult CreateBlogPostCategory()
+        {
+            return View();
+        }
+    }
+}
