290d2939793b027e5ead91e35d8fe574f9563f0a
ilyo_bilyo <ilito20000@gmail.com>
2022-09-24 14:37:48 +0300

    GetAllSortBlogPostsForPageing in BlogService now retirn one model PagedListServiceModel

--- CookDelicious/CookDelicious.Core/Contracts/Blog/IBlogService.cs
+++ CookDelicious/CookDelicious.Core/Contracts/Blog/IBlogService.cs
@@ -4,7 +4,7 @@ namespace CookDelicious.Core.Contracts.Blog
 {
     public interface IBlogService
     {
-        Task<(IEnumerable<BlogPostServiceModel>, int)> GetAllSortBlogPostsForPageing(int pageNumber, int pageSize, string blogPostCategory, int? sortMonth);
+        Task<PagedListServiceModel<BlogPostServiceModel>> GetAllSortBlogPostsForPageing(int pageNumber, int pageSize, string blogPostCategory, int? sortMonth);
         Task<IList<string>> GetBlogAllPostCategoryNames();
         Task<IList<string>> GetBlogArchive();
         Task<BlogPostServiceModel> GetBlogPostServiceModelById(Guid id);
