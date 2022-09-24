db70a43e6059a67701b603314efaf693c5ccc8a6
ilyo_bilyo <ilito20000@gmail.com>
2022-09-24 14:42:15 +0300

    Create PageingServiceAdmin

---
+++ CookDelicious/CookDelicious.Core/Services/Admin/PageingServiceAdmin.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using CookDelicious.Core.Contracts.Admin;
+using CookDelicious.Core.Models.Admin;
+using CookDelicious.Core.Models.Paiging;
+
+namespace CookDelicious.Core.Services.Admin
+{
+    public class PageingServiceAdmin : IPageingServiceAdmin
+    {
+        private readonly IUserServiceAdmin userService;
+        private readonly IMapper mapper;
+
+        public PageingServiceAdmin(IUserServiceAdmin userService,
+            IMapper mapper)
+        {
+            this.userService = userService;
+            this.mapper = mapper;
+        }
+
+        public async Task<PagingList<UserListViewModel>> GetAllUsersForManegment(int pageNumber)
+        {
+            if (pageNumber == 0)
+            {
+                pageNumber = 1;
+            }
+
+            int pageSize = 2;
+
+            var usersPagedListServiceModels = await userService.GetUsersPageingInManageUsers(pageNumber, pageSize);
+
+            var usersViewModel = mapper.Map<List<UserListViewModel>>(usersPagedListServiceModels.Items);
+
+            var pageingList = new PagingList<UserListViewModel>(usersViewModel, usersPagedListServiceModels.TotalCount, pageNumber, pageSize);
+
+            return pageingList;
+        }
+    }
+}
