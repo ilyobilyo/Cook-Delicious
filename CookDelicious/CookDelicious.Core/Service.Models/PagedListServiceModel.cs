ce4adcbdc874d9eeb89ad100dc0f60769c10ad2b
ilyo_bilyo <ilito20000@gmail.com>
2022-09-24 14:40:08 +0300

    Create PagedListServiceModel<T>

---
+++ CookDelicious/CookDelicious.Core/Service.Models/PagedListServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CookDelicious.Core.Service.Models
+{
+    public class PagedListServiceModel<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+}
