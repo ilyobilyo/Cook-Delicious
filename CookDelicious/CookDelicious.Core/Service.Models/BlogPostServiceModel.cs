28364c7496b073e813c4e83a744137e082231424
ilyobilyo <ilito20000@gmail.com>
2022-09-25 14:39:03 +0300

    Add attributes to CreateProductInputModel

--- CookDelicious/CookDelicious.Core/Service.Models/BlogPostServiceModel.cs
+++ CookDelicious/CookDelicious.Core/Service.Models/BlogPostServiceModel.cs
@@ -2,7 +2,7 @@
 {
     public class BlogPostServiceModel
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; }
 
         public string Title { get; set; }
 
--- CookDelicious/CookDelicious.Core/Service.Models/InputServiceModels/CreateProductInputModel.cs
+++ CookDelicious/CookDelicious.Core/Service.Models/InputServiceModels/CreateProductInputModel.cs
@@ -1,9 +1,15 @@
-﻿namespace CookDelicious.Core.Service.Models.InputServiceModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookDelicious.Core.Service.Models.InputServiceModels
 {
     public class CreateProductInputModel
     {
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string Type { get; set; }
 
         public string ImageUrl { get; set; }
